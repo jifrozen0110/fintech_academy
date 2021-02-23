d01bce97708355a3a76cd426f7ad6d61bf50d5f0
gwanwoo-you <skymill2000@gmail.com>
2021-02-23 16:29:17 +0900

    사용자 조회 기능 작성

--- expressServer.js
+++ expressServer.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const app = express()
- 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '1q2w3e4r',
+  database : 'fintech210222',
+});
+connection.connect();
+// connection.end();
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
 app.get('/user', function (req, res) {
-  res.send('사용자 페이지 입니다.')
+    connection.query('SELECT * FROM user;', function (error, results, fields) {
+        console.log(results);
+        res.send(results)
+    });
 })
  
 app.listen(3000)
