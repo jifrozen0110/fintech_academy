f95792ed95db15f9924005626e513727eb50a59a
gwanwoo-you <skymill2000@gmail.com>
2021-02-23 16:43:14 +0900

    ejs 모듈 추가 ejs 샘플 파일 추가

--- expressServer.js
+++ expressServer.js
@@ -1,5 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+app.set('views', __dirname + '/views');
+//뷰파일이 있는 디렉토리를 설정합니다
+app.set('view engine', 'ejs');
+//뷰엔진으로 ejs 사용한다 
+
+// connection.end();
+app.get('/', function (req, res) {
+  res.send('Hello World');
+})
+
+app.get('/user', function (req, res) {
+    connection.query('SELECT * FROM user;', function (error, results, fields) {
+        console.log(results);
+        res.send(results);
+    });
+})
+
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -7,17 +25,8 @@ var connection = mysql.createConnection({
   password : '1q2w3e4r',
   database : 'fintech210222',
 });
-connection.connect();
-// connection.end();
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
-app.get('/user', function (req, res) {
-    connection.query('SELECT * FROM user;', function (error, results, fields) {
-        console.log(results);
-        res.send(results)
-    });
-})
- 
+connection.connect();
+
+
 app.listen(3000)
