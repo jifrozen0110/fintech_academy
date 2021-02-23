79586763c09f2a2a97d0dc280bb5f2fad7ca50e7
gwanwoo-you <skymill2000@gmail.com>
2021-02-23 14:30:23 +0900

    #work3 오브젝트 데이터 찾기 예제

--- requestExample.js
+++ requestExample.js
@@ -4,6 +4,7 @@ var xml = undefined;
 request('http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109', function (error, response, body) {
   xml = body;
   parseString(xml, function (err, result) {
-    console.dir(result.rss.channel);
+    console.dir(result.rss.channel[0].item[0].description[0].header[0].wf);
+    //#work3 wf 데이터 조회후 출력하기
   });
 });
