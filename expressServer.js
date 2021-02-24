670df8bc172b12b4f5bcc4563e04acbf00a45dc1
gwanwoo-you <skymill2000@gmail.com>
2021-02-24 16:11:04 +0900

    로그인 페이지 추가

--- expressServer.js
+++ expressServer.js
@@ -24,6 +24,10 @@ app.get('/signup', function(req, res){
     res.render('signup');
 })
 
+app.get('/login', function(req, res){
+    res.render('login');
+})
+
 app.get('/authResult', function(req, res){
     var authCode = req.query.code;
     var option = {
